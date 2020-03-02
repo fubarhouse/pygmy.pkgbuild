2a32fdd0cebcd9f9dca4ca0eaf0058fcbc915957
Karl <karl.hepworth@gmail.com>
2020-03-02 11:56:59 +1100

    Add a docker build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# To be run from inside the ArchLinux container.
+
+# Set working directory
+cd /app || exit
+
+# Update
+pacman -Sy
+
+# Install dependencies
+pacman -Sy --noconfirm base-devel
+
+# Build pygmy-go-git
+cd /app/pygmy-go-git || exit
+sudo -u nobody makepkg -Si
+
+# Build pygmy-go
+cd /app/pygmy-go || exit
+sudo -u nobody makepkg -Si
+
+# Cleanup
+pacman -Sc --noconfirm
