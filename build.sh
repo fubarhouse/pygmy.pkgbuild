9a4491818fb11b1350924e3bb21234a6eea962f0
Karl Hepworth <karl.hepworth@gmail.com>
2022-01-05 22:28:13 +1100

    force rebuild in build script
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- build.sh
+++ build.sh
@@ -12,11 +12,11 @@ pacman -Sy --noconfirm base-devel git go-pie
 
 # Build pygmy-go-git
 cd /app/pygmy-go-git || exit
-sudo -u nobody makepkg -Si
+sudo -u nobody makepkg -Sfi
 
 # Build pygmy-go-bin
 cd /app/pygmy-go-bin || exit
-sudo -u nobody makepkg -Si
+sudo -u nobody makepkg -Sfi
 
 # Cleanup
 pacman -Sc --noconfirm
